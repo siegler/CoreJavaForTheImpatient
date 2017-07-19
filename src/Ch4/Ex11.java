e264af35a23fb1e95abba59f5db34b3c0022fffc
Chris Siegler <sieglerc@gmail.com>
2017-07-19 11:59:06 -0500

    Modified Exercise 11

--- src/Ch4/Ex11.java
+++ src/Ch4/Ex11.java
@@ -1,6 +1,5 @@
 package Ch4;
 
-import java.io.PrintStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
@@ -10,14 +9,10 @@ Write the “Hello, World” program, using reflection to look up the out field 
 public class Ex11 {
     public static void main(String[] args) {
         try {
-            Field out = java.lang.System.class.getField("out");
-            System.out.println(out);
+            Field field = java.lang.System.class.getField("out");
+            Object out = field.get(null); //static class
             Method print = out.getClass().getMethod("println", String.class);
-            Method m = java.io.PrintStream.class.getMethod("println", String.class);
-            System.out.println(m);
-            PrintStream ps = new PrintStream(System.out);
-//            m.invoke(ps, "Hello, World!");
-            print.invoke(ps, "Hello, world!");
+            print.invoke(out, "Hello, world!");
         } catch (Exception e) {
             e.printStackTrace();
         }
