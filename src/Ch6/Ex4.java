36a6418223fd9453abd0f26f3623672e8498c457
Chris Siegler <sieglerc@gmail.com>
2017-07-29 13:56:47 -0500

    Added exercise

--- src/Ch6/Ex4.java
+++ src/Ch6/Ex4.java
@@ -1,8 +1,11 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+package Ch6;
 
-class Table<K, V> {
-    private ArrayList<Entry<K,V>> entries = new ArrayList<>();
+import java.util.ArrayList;
+/*
+In the previous exercise, make Entry into a nested class. Should that class be generic?
+ */
+class Table2<K, V> {
+    private ArrayList<Entry> entries = new ArrayList<>();
     public V get(K key) {
         for (Entry entry: entries) {
             if (entry.getKey() == key)
@@ -19,7 +22,7 @@ class Table<K, V> {
         //new key
         entries.add(new Entry(key, value));
     }
-    public Entry<K,V> remove(K key) {
+    public Entry remove(K key) {
         for (int i = 0; i < entries.size(); i++) {
             if (entries.get(i).getKey() == key) {
                 return entries.remove(i);
@@ -27,43 +30,27 @@ class Table<K, V> {
         }
         return null;
     }
-}
 
-class Entry<K, V> implements Comparable {
-    private K key;
-    private V value;
-
-    public Entry(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public K getKey() {
-        return key;
-    }
-
-    public V getValue() {
-        return value;
-    }
-
-    public void setValue(V value) {
-        this.value = value;
-    }
-
-    @Override
-    public int compareTo(Object o) {
-        return this.key == ((Entry<K,V>)o).getKey() ? 1: 0;
+    class Entry {
+        private K key;
+        private V value;
+        Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+        public K getKey() { return key; }
+        public V getValue() { return value; }
+        public void setValue(V value) {
+            this.value = value;
+        }
     }
 }
-
-public class Ex3 {
+public class Ex4 {
     public static void main(String[] args) {
-        Table<String,Integer> stooges = new Table<>();
+        Table2<String,Integer> stooges = new Table2<>();
         stooges.put("Moe",1);
         stooges.put("Curly",2);
         stooges.put("Shemp",3);
         System.out.println(stooges.get("Curly"));
-        Entry<String,Integer> stooge = stooges.remove("Shemp");
-        System.out.println(stooge.getKey());
     }
 }
