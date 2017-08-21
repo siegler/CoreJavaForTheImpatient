d13bb06831f27355f8cda18213154a5cb7321962
Chris Siegler <sieglerc@gmail.com>
2017-08-21 14:14:30 -0500

    Added exercise

---
+++ src/Ch7/Ex9.java
@@ -0,0 +1,62 @@
+package Ch7;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Ex9 {
+    public static void printMap(Map map) {
+        map.forEach((k,v) -> System.out.println(k + ": " + v));
+    }
+    public static void main(String[] args) {
+        String words = "one two three one two one";
+        Map<String, Integer> counts = new HashMap<>();
+
+        //with contains
+        for (String word: words.split(" ")) {
+            if (counts.containsKey(word)) {
+                int count = counts.get(word);
+                counts.put(word, ++count);
+            } else {
+                counts.put(word, 1);
+            }
+        }
+
+        printMap(counts);
+
+        counts.clear();
+
+        //by using get and a null check
+        for (String word: words.split(" ")) {
+            if (counts.get(word) == null) {
+                counts.put(word, 1);
+            } else {
+                int count = counts.get(word);
+                counts.put(word, ++count);
+            }
+        }
+
+        printMap(counts);
+
+        counts.clear();
+
+        //by using getOrDefault
+        for (String word: words.split(" ")) {
+            int count = counts.getOrDefault(word, 0);
+            counts.put(word, ++count);
+        }
+
+        printMap(counts);
+
+        counts.clear();
+        //by using putIfAbsent
+        for (String word: words.split(" ")) {
+            counts.putIfAbsent(word, 0);
+            int count = counts.get(word);
+            counts.put(word, ++count);
+        }
+
+        printMap(counts);
+
+    }
+}
