5ab9e20adfbc0c745bfca071a0052b2b3983242e
Chris Siegler <sieglerc@gmail.com>
2017-08-23 16:33:14 -0500

    Added exercise

---
+++ src/Ch7/Ex13.java
@@ -0,0 +1,24 @@
+package Ch7;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+/*
+The LinkedHashMap calls the method removeEldestEntry whenever a new element is inserted. Implement a subclass Cache that limits the map to a given size provided in the constructor.
+ */
+class Cache extends LinkedHashMap {
+    private final int capacity;
+    public Cache(int initialCapacity) {
+        super(initialCapacity);
+        capacity = initialCapacity;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry eldest) {
+        return size() > capacity;
+    }
+}
+public class Ex13 {
+    public static void main(String[] args) {
+        Cache map = new Cache(2);
+    }
+}
