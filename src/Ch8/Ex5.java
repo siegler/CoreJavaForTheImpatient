483e4eccd8d61c68fcb628e8a2db4c35f6773a9e
Chris Siegler <sieglerc@gmail.com>
2017-11-19 23:28:54 -0600

    Added exercise

---
+++ src/Ch8/Ex5.java
@@ -0,0 +1,20 @@
+package Ch8;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/*
+The codePoints method in Section 8.3, “The filter , map , and flatMap Methods”
+(page 263) was a bit clumsy, first filling an array list and then turning
+it into a stream. Write a stream-based version instead, using the
+IntStream.iterate method to construct a finite stream of offsets, then extract
+the substrings.
+ */
+public class Ex5 {
+    public static void main(String[] args) {
+        String s = "boat\u03bb";
+        Stream<String> result = IntStream.range(0, s.length()).mapToObj(i -> s.substring(i, s.offsetByCodePoints(i, 1)));
+        System.out.println(Arrays.toString(result.toArray()));
+    }
+}
